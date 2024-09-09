b59eb3fbb4a77811dbe682854d5c88f0e23ee6bf
Cristina Aguilera <developer.c.93.c@gmail.com>
2024-09-09 13:46:30 +0200

    mejorando css

---
+++ js/Espe.js
@@ -0,0 +1,27 @@
+class Esperanza {
+
+    constructor(){
+
+        this.x = 50
+        this.y = 50
+        this.h = 35
+        this.w = 40
+
+        this.speedMovement = 20
+
+        //crear Espe en DOM
+        this.espeNode = document.createElement("img")
+        this.espeNode.src = "./img/cochePrueba.png"
+        pantallaJuego.append(this.espeNode)
+
+    }
+
+    
+
+
+
+
+
+
+
+}
